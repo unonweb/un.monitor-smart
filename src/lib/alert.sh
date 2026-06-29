56dc10153142a27bb04d9d9f9d4b13f8d6bb6fcb
unonweb <unonweb@posteo.de>
2026-06-29 23:37:08 +0200

    enhance alert function

--- src/lib/alert.sh
+++ src/lib/alert.sh
@@ -2,10 +2,18 @@ function alert {
     local subject="${1}"
     local message="${2}"
 
-    if ((ALERT_MAIL)); then
-		echo "Sending Mail-Alert to ${ALERT_MAIL_TO}"
+	if (( ALERT_MAIL )); then
 
-		echo -e "${message}" | \
-		mail -s "${ALERT_MAIL_SUBJECT} ${subject}" "${ALERT_MAIL_TO}"
+		if [[ -z "${ALERT_MAIL_TO}" ]]; then
+			log "<3> Required var not set: ALERT_MAIL_TO"
+			return 1
+		else
+			log "<6> Sending Mail-Alert to ${ALERT_MAIL_TO}"
+
+			echo -e "${message}" | \
+			mail -s "${ALERT_MAIL_SUBJECT} ${subject}" "${ALERT_MAIL_TO}" \
+			&& log "<5> Mail-Alert sent to ${ALERT_MAIL_TO}" \
+			&& return 0
+		fi
 	fi
 }
